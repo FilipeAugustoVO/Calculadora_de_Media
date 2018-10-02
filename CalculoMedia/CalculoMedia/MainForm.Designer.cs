02ef7672177cb6a278a53498b967b9a3b85ac68e
FilipeAugustoVO <43390392+FilipeAugustoVO@users.noreply.github.com>
2018-10-02 15:01:15 -0300

    Add files via upload

---
+++ CalculoMedia/CalculoMedia/MainForm.Designer.cs
@@ -0,0 +1,274 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Convidado
+ * Date: 12/09/2018
+ * Time: 16:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace CalculoMedia
+{
+	partial class FormaCalculo1
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+		private System.Windows.Forms.Label label6;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label6 = new System.Windows.Forms.Label();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(93, 15);
+			this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(471, 63);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(162, 33);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(338, 22);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.Text = "Média do Aluno (Gráfico temporário)";
+			this.textBox1.TextChanged += new System.EventHandler(this.TextBox1TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(16, 149);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(94, 22);
+			this.textBox2.TabIndex = 2;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+			"2",
+			"3",
+			"5"});
+			this.comboBox1.Location = new System.Drawing.Point(16, 181);
+			this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(94, 24);
+			this.comboBox1.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(16, 124);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(94, 21);
+			this.label1.TabIndex = 4;
+			this.label1.Text = "Primeira Nota";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.label1.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(128, 124);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(114, 21);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Segunda Nota";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.label2.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// label4
+			// 
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(263, 124);
+			this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(94, 21);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Trabalho";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.label4.Click += new System.EventHandler(this.Label1Click);
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(137, 149);
+			this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(94, 22);
+			this.textBox3.TabIndex = 2;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+			"2",
+			"3",
+			"5"});
+			this.comboBox2.Location = new System.Drawing.Point(137, 181);
+			this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(94, 24);
+			this.comboBox2.TabIndex = 3;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(263, 149);
+			this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(94, 22);
+			this.textBox4.TabIndex = 2;
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Items.AddRange(new object[] {
+			"2",
+			"3",
+			"5"});
+			this.comboBox3.Location = new System.Drawing.Point(263, 181);
+			this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(94, 24);
+			this.comboBox3.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(10, 221);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(72, 23);
+			this.label3.TabIndex = 5;
+			this.label3.Text = "Qde Aulas";
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(10, 254);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(83, 23);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "Qde Faltas";
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(88, 218);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 22);
+			this.textBox5.TabIndex = 6;
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(88, 251);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 22);
+			this.textBox6.TabIndex = 6;
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(263, 251);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+			this.numericUpDown1.TabIndex = 7;
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(263, 225);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(100, 23);
+			this.label6.TabIndex = 8;
+			this.label6.Text = "Nota de Corte";
+			// 
+			// FormaCalculo1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(645, 446);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.numericUpDown1);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.pictureBox1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "FormaCalculo1";
+			this.Text = "CalculoMedia";
+			this.Load += new System.EventHandler(this.FormaCalculo1Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ CalculoMedia/CalculoMedia/MainForm.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Convidado
+ * Date: 12/09/2018
+ * Time: 16:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace CalculoMedia
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class FormaCalculo1 : Form
+	{
+		public FormaCalculo1()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void TextBox1TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void TextBox3TextChanged(object sender, EventArgs e)
+		{
+	
+		}
+		void Label1Click(object sender, EventArgs e)
+		{
+	
+		}
+		void FormaCalculo1Load(object sender, EventArgs e)
+		{
+	
+		}
+	}
+}
